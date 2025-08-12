9a8b2bcedcffca86dea74a46190786c071400983
Wilaak <54738571+Wilaak@users.noreply.github.com>
2025-08-12 18:48:09 +0200

    Hello, World!

---
+++ README.md
@@ -0,0 +1,86 @@
+# Datastar PHP Swoole SDK
+
+This package provides an SDK for using [Datastar](https://data-star.dev) with [SwoolePHP](https://wiki.swoole.com/en/).
+
+SwoolePHP allows you to build efficient, high-concurrency applications.
+
+## Installation
+
+    composer require wilaak/datastar-php-swoole
+
+## Usage Examples
+
+In the SwoolePHP HTTP Server each request is put in a [coroutine](https://wiki.swoole.com/en/#/start/coroutine), allowing you to use standard blocking PHP code without troubles.
+
+```PHP
+use Swoole\Http\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Wilaak\Datastar\Swoole\ServerSentEventGenerator;
+
+$http = new Server("0.0.0.0", 8082);
+
+$http->on('request', function (Request $request, Response $response) {
+    $sse = new ServerSentEventGenerator($request, $response);
+
+    $message = "Hello, World!";
+    foreach (str_split($message) as $i => $char) {
+        $sse->patchElements("<h3 id='message'>" . substr($message, 0, $i + 1) . "</h3>");
+        sleep(1);
+    }
+});
+
+$http->start();
+```
+
+```php
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Swoole\Http\Server;
+use Wilaak\Datastar\Swoole\ServerSentEventGenerator;
+use starfederation\datastar\enums\ElementPatchMode;
+
+$http = new Server("0.0.0.0", 8082);
+
+$http->on('request', function (Request $request, Response $response) {
+    // Creates a new `ServerSentEventGenerator` instance.
+    $sse = new ServerSentEventGenerator($request, $response);
+
+    // Reads signals from the request.
+    $signals = $sse->readSignals();
+
+    // Patches elements into the DOM.
+    $sse->patchElements('<div></div>', [
+        'selector' => '#my-div',
+        'mode' => ElementPatchMode::Append,
+        'useViewTransition' => true,
+    ]);
+
+    // Removes elements from the DOM.
+    $sse->removeElements('#my-div', [
+        'useViewTransition' => true,
+    ]);
+
+    // Patches signals.
+    $sse->patchSignals(['foo' => 123], [
+        'onlyIfMissing' => true,
+    ]);
+
+    // Executes JavaScript in the browser.
+    $sse->executeScript('console.log("Hello, world!")', [
+        'autoRemove' => true,
+        'attributes' => [
+            'type' => 'application/javascript',
+        ],
+    ]);
+
+    // Redirects the browser by setting the location to the provided URI.
+    $sse->location('/guide');
+});
+
+$http->start();
+```
+
+## License
+
+MIT
---
+++ src/ServerSentEventGenerator.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Wilaak\Datastar\Swoole;
+
+use starfederation\datastar\enums\ElementPatchMode;
+use starfederation\datastar\events\EventInterface;
+use starfederation\datastar\events\ExecuteScript;
+use starfederation\datastar\events\Location;
+use starfederation\datastar\events\PatchElements;
+use starfederation\datastar\events\PatchSignals;
+use starfederation\datastar\events\RemoveElements;
+use starfederation\datastar\Consts;
+
+class ServerSentEventGenerator
+{
+    public bool $headersSent = false;
+
+    public function __construct(
+        private \Swoole\Http\Request $request,
+        private \Swoole\Http\Response $response
+    ) {}
+
+    /**
+     * Returns the signals sent in the incoming request.
+     */
+    public function readSignals(): array
+    {
+        $input = $this->request->get[Consts::DATASTAR_KEY] ?? $this->request->rawContent();
+        $signals = $input ? json_decode($input, true) : [];
+
+        return is_array($signals) ? $signals : [];
+    }
+
+    /**
+     * Sends the response headers using the Swoole HTTP response object, if not already sent.
+     */
+    public function sendHeaders(): void
+    {
+        if ($this->headersSent) {
+            return;
+        }
+
+        $headers = [
+            'Cache-Control' => 'no-cache',
+            'Content-Type' => 'text/event-stream',
+            // Disable buffering for Nginx.
+            // https://nginx.org/en/docs/http/ngx_http_proxy_module.html#proxy_buffering
+            'X-Accel-Buffering' => 'no',
+        ];
+
+        // Connection-specific headers are only allowed in HTTP/1.1.
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
+        $proto = $this->request->server['server_protocol'] ?? 'HTTP/1.1';
+        if ($proto === 'HTTP/1.1') {
+            $headers['Connection'] = 'keep-alive';
+        }
+
+        foreach ($headers as $name => $value) {
+            $this->response->header($name, $value);
+        }
+        $this->headersSent = true;
+    }
+
+    /**
+     * Patches HTML elements into the DOM and returns the resulting output.
+     *
+     * @param array{
+     *     selector?: string|null,
+     *     mode?: ElementPatchMode|string|null,
+     *     useViewTransition?: bool|null,
+     *     eventId?: string|null,
+     *     retryDuration?: int|null,
+     * } $options
+     */
+    public function patchElements(string $elements, array $options = []): string
+    {
+        return $this->sendEvent(new PatchElements($elements, $options));
+    }
+
+    /**
+     * Patches signals and returns the resulting output.
+     */
+    public function patchSignals(array|string $signals, array $options = []): string
+    {
+        return $this->sendEvent(new PatchSignals($signals, $options));
+    }
+
+    /**
+     * Removes elements from the DOM and returns the resulting output.
+     *
+     * @param array{
+     *      eventId?: string|null,
+     *      retryDuration?: int|null,
+     *  } $options
+     */
+    public function removeElements(string $selector, array $options = []): string
+    {
+        return $this->sendEvent(new RemoveElements($selector, $options));
+    }
+
+    /**
+     * Executes JavaScript in the browser and returns the resulting output.
+     */
+    public function executeScript(string $script, array $options = []): string
+    {
+        return $this->sendEvent(new ExecuteScript($script, $options));
+    }
+
+    /**
+     * Redirects the browser by setting the location to the provided URI and returns the resulting output.
+     */
+    public function location(string $uri, array $options = []): string
+    {
+        return $this->sendEvent(new Location($uri, $options));
+    }
+
+    /**
+     * Sends an event using the Swoole HTTP response object and returns the resulting output.
+     */
+    protected function sendEvent(EventInterface $event): string
+    {
+        if (!$this->headersSent) {
+            $this->sendHeaders();
+        }
+        $output = $event->getOutput();
+        $this->response->write($output);
+        return $output;
+    }
+}
